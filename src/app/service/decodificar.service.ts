7ccd8b92c3bee50f0c3bee8b0344a3259b95c88d
Júlia Assis <julia.bhte@gmail.com>
2023-04-12 22:01:41 -0300

    projeto maraton

--- src/app/service/decodificar.service.ts
+++ src/app/service/decodificar.service.ts
@@ -13,14 +13,14 @@ export class DecodificarService {
 
   getCifraCesar(value:string,chave:string):string{
     
-    return null;
+    return this.decriptografarCesar(value.toUpperCase(),chave.toUpperCase());
   }
   getCifraVigenere(value:string,chave:string):string{
 
     return  this.decriptografarVigenere(value.toUpperCase(),chave.toUpperCase());
   }
   getCercaTrilho(value:string,chave:string):string{
-    return null;
+    return this.decriptografarCercaTrilho(value.toUpperCase(),chave.toUpperCase());
   }
 
   //funcao para achar a posicao do caracter no array
@@ -44,6 +44,77 @@ export class DecodificarService {
   let texto_decriptografado = '';
   for(let i = 0; i < texto_para_descriptografar.length; i = i + 1) {
       texto_decriptografado += this.backCaracterVigenere(texto_para_descriptografar[i], chave[i])
+  }
+  
+  return texto_decriptografado;
+}
+
+ dividirTextoEmColunas(texto,chave){
+  /**
+   * a quantidade de colunas vai ser igual a:
+   * a quantidade de caracteres da mensagem
+   * dividida pela quantidade de números da chave
+   * arredondado para o numero inteiro a cima, caso não seja uma divisão exata
+   */
+  let tamanhoColunas = Math.ceil(texto.length / chave.length);
+  let textoDividido = [];
+  
+  for(let i = 0; i < chave.length; i = i + 1) {
+      textoDividido.push(texto.slice(i*tamanhoColunas, i*tamanhoColunas+tamanhoColunas));
+  }
+
+  return textoDividido;
+}
+
+//função principal que descriptografa a mensagem
+ decriptografarCercaTrilho(texto_para_descriptografar, chave){
+  let texto_dividido = this.dividirTextoEmColunas(texto_para_descriptografar, chave);
+  let texto_decriptografado = '';
+
+  /**
+   * um loop para pegar os caracteres seguindo a ordem da chave
+   */
+  for (let i = 0; i < texto_dividido[0].length; i = i + 1){
+      for(let j = 0; j < chave.length; j = j + 1){
+          if(texto_dividido[chave[j]-1][i]){
+              texto_decriptografado += texto_dividido[chave[j]-1][i]
+          }
+          else{
+              texto_para_descriptografar += ' ';
+          }
+      }
+  }
+
+  return texto_decriptografado;
+}
+changeCaracter(caracter, chave){
+  let index = this.findIndex(this.alfabeto, caracter) + chave;
+
+  /**
+   * se na subtracao, que e quando faz a decodificacao do texto, der um valor menor que 37,
+   * fazer o modulo por 37 e somar com 37 para dar o correspondente a ele no valor positivo
+   * e que pertanca ao array base do alfabeto
+   */
+  if (index < 0){
+      index = index % 37 + 37;
+  }
+
+  /**
+   * se na soma, que normalmente é quando faz a codificacao do texto, der um valor maior que 37,
+   * fazer o modulo por 37 que vai dar o valor correspondente dentro dos limites do array base
+   */
+  if (index > 36){
+      index = index % 37;
+  }
+  
+  return this.alfabeto[index];
+}
+//funcao principal que decriptografa a mensagem
+ decriptografarCesar(texto_para_descriptografar, chave){
+  let texto_decriptografado = '';
+  //loop para mudar todos os caracteres, um a um, com base na chave digitada
+  for(let i = 0; i < texto_para_descriptografar.length; i = i + 1) {
+      texto_decriptografado += this.changeCaracter(texto_para_descriptografar[i], -chave)
   }
   
   return texto_decriptografado;
